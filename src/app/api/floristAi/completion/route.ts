f256c2e2fa80fc01ddcb1b7a7440c2145dc47b33
billsusanto <billsusanto01@gmail.com>
2025-02-22 17:19:51 -0800

    Initial commit

---
+++ src/app/api/floristAi/completion/route.ts
@@ -0,0 +1,43 @@
+import { openai } from "@ai-sdk/openai";
+import { experimental_generateImage, Message, streamText, tool } from "ai";
+import { z } from "zod";
+
+export async function POST(request: Request) {
+  const { messages }: { messages: Message[] } = await request.json();
+
+  // filter through messages and remove base64 image data to avoid sending to the model
+  const formattedMessages = messages.map((m) => {
+    if (m.role === "assistant" && m.toolInvocations) {
+      m.toolInvocations.forEach((ti) => {
+        if (ti.toolName === "generateImage" && ti.state === "result") {
+          ti.result.image = `redacted-for-length`;
+        }
+      });
+    }
+    return m;
+  });
+
+  const result = streamText({
+    model: openai("gpt-4o"),
+    messages: formattedMessages,
+    tools: {
+      generateImage: tool({
+        description: "Generate an image",
+        parameters: z.object({
+          prompt: z.string().describe("Make your images black and white"),
+        }),
+        execute: async ({ prompt }) => {
+          const { image } = await experimental_generateImage({
+            model: openai.image("dall-e-3"),
+            prompt,
+          });
+          // in production, save this image to blob storage and return a URL
+          return { image: image.base64, prompt };
+        },
+      }),
+    },
+    toolCallStreaming: true,
+  });
+
+  return result.toDataStreamResponse();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { SendHorizonalIcon } from "lucide-react";
+import { FormEvent, useState, useEffect, useRef } from "react";
 import Image from "next/image";
+import "./styles/chatAnimations.css";
 
-export default function Home() {
+export default function Chat() {
+  const { messages, input, setInput, handleInputChange, handleSubmit } =
+    useChat({
+      api: "/api/floristAi/completion",
+    });
+
+  const stripFormatting = (text: string): string => {
+    return text.replace(/(\*)/g, "");
+  };
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const customHandleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSubmit(e);
+    setInput("");
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col mt-14 items-center w-[100%] h-[80vh]">
+      <div className="flex justify-center items-center text-center text-4xl text-black">
+        <span className="hidden sm:block">Welcome to Flowershop.ai</span>
+        <div className="hidden sm:block"></div>
+        <span className="block sm:hidden">Hey there!</span>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <p className="text-left mt-6 text-gray-500 text-xl">
+        Generate AI-powered images with custom prompts!
+      </p>
+
+      <div className="mt-4 w-[80%] sm:w-[60%] text-md">
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={`my-2 text-md py-4 px-8 rounded-lg w-fit ${
+              m.role === "user"
+                ? "ml-auto bg-blue-100 text-blue-900 slide-in-right"
+                : "mr-auto mb-10 bg-gray-100 text-gray-900 slide-in-left"
+            }`}
+            style={{ whiteSpace: "pre-line" }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {m.toolInvocations
+              ? m.toolInvocations.map((ti) =>
+                  ti.toolName === "generateImage" ? (
+                    ti.state === "result" ? (
+                      <div key={ti.toolCallId}>
+                        <Image
+                          src={`data:image/png;base64,${ti.result.image}`}
+                          alt={ti.result.prompt}
+                          width={400}
+                          height={400}
+                          className="max-w-[512px] rounded-lg mb-2"
+                        />
+                        <p>{stripFormatting(ti.result.prompt)}</p>
+                      </div>
+                    ) : (
+                      <div key={ti.toolCallId} className="animate-pulse">
+                        Generating image...
+                      </div>
+                    )
+                  ) : null
+                )
+              : stripFormatting(m.content)}
+          </div>
+        ))}
+      </div>
+
+      <form
+        ref={formRef}
+        className="flex py-4 px-8 w-[80%] sm:w-[60%] border-2 border-black rounded-xl mb-0 mt-10"
+        onSubmit={customHandleSubmit}
+      >
+        <input
+          name="prompt"
+          className="h-8 w-[100%] text-black border-black focus:outline-none"
+          value={input}
+          onChange={handleInputChange}
+          id="prompt"
+          placeholder="Enter your prompt..."
+        />
+        <button type="submit">
+          <SendHorizonalIcon className="text-black" />
+        </button>
+      </form>
     </div>
   );
 }
